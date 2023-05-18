5f6411896c9f7b3c970dfc60f91443cf4cceb409
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2023-05-18 08:35:24 -0300

    Criação da arquitetura base.

---
+++ Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
